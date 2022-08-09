84bc85d8e8becf2f5228fc4f0adc020b8c3f7070
Lucas52352 <lucasrius86@gmail.com>
2022-08-08 22:58:28 -0300

    Ex 1.1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,28 @@
+import React, { Components } from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Total from './Components/Total';
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]} />
+      <Total sum={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+
+
+export default App;
---
+++ part1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ReactDOM } from 'react'
+
+const App = () => {
+  console.log('Hello from component')
+  return (
+    <div>
+      <p> Hello world</p>
+    </div>
+  )
+}
+
+export default App
