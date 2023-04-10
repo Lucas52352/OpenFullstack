7c49e539acbc9fc943c6cd8a0032952ad2dd8fdc
Lucas52352 <lucasrius86@gmail.com>
2023-04-09 23:29:51 -0300

    small fixes

--- part2/coursepart2/data-for-countries/src/Components/country.js
+++ part2/coursepart2/data-for-countries/src/Components/country.js
@@ -4,47 +4,63 @@ import axios from "axios";
 
 
 const Country = () => {
+
     const [countryData, setCountryData] = useState({})
     const [flag, setFlag] = useState('')
+
     const [name, setName] = useState('')
     const [languages, setLanguages] = useState([])
+
     const [capital, setCapital] = useState('')
+    const routeMatch = useMatch('/country/:name')
+
+    const country = routeMatch.params.name
+    const navigate = useNavigate()
+
+    const api_key = process.env.REACT_APP_API_KEY
     const [weatherData, setWeatherData] = useState({
         temperature:'',
         direction:'', 
         velocity:'',
         img:''
     })
-    const routeMatch = useMatch('/country/:name')
-    const country = routeMatch.params.name
-    const navigate = useNavigate()
-    const api_key = process.env.REACT_APP_API_KEY
 
     useEffect (()=>{
+
         const fetchCountry = async () => {
+
           const response = await axios.get(`https://restcountries.com/v3.1/name/${country}?fullText=true`) 
+
             setCountryData(response.data[0])    
             setName(response.data[0].name.common)
+
             setFlag(response.data[0].flags.png)
             setLanguages(response.data[0].languages) 
+
             setCapital(response.data[0].capital)
+
           const weatherResponse = await axios.get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${response.data[0].capital}`)
+
             setWeatherData({
+
                 temperature: weatherResponse.data.current.temperature,
                 direction: weatherResponse.data.current.wind_dir,
+
                 velocity: weatherResponse.data.current.wind_speed,
                 img: weatherResponse.data.current.weather_icons
-                
-                
             })
             
         }
+
         fetchCountry();    
     }, [])
 
     return (
+
         <div>
+
             <button onClick={()=>navigate(-1)}>Go Back Home</button>
+
             <h3> Country: </h3>
             {name} <br/>
 
@@ -60,16 +76,21 @@ const Country = () => {
                     • {language}
                 </li>
             )}
+
             <h3>Flag:</h3>
             <img src={flag} alt='flags.png'></img> <br/>
 
             <h3> Wheather in {capital}:</h3>
+        
             <p>
                 Temperature: {weatherData.temperature}°C <br/>
+
                 <img src={weatherData.img} alt='weather icon'></img> <br/>
+
                 Wind:
                 {weatherData.velocity} mph 
                 {weatherData.direction}
+                
             </p>
             
         </div>
--- part2/coursepart2/data-for-countries/src/Components/countryList.js
+++ part2/coursepart2/data-for-countries/src/Components/countryList.js
@@ -6,46 +6,57 @@ import { useNavigate } from "react-router-dom";
 
 const CountryList = () => {
 
-    const [countries, setCountries] = useState([]) 
-    const [searchField, setSearchField] = useState('')
-    const navigate = useNavigate()
+  const [countries, setCountries] = useState([]) 
+  const [searchField, setSearchField] = useState('')
+  const navigate = useNavigate()
 
   
-      useEffect(()=>{ 
+  useEffect(()=>{ 
+
     const fetchCountries = async () =>{
     if (!searchField) {return}
+
     const response = await axios.get(`https://restcountries.com/v3.1/name/${searchField}`)
     setCountries(response.data)
-  };
-  
+
+    };
+
     fetchCountries()
-    }, [searchField])
+  }, [searchField])
+
+  const handleChange = event => {
+    setSearchField(event.target.value);
+  }
+
+  const handleOnClick = (name) => {
+    navigate(`../country/${name}`)
+  }
+
+  return(
+    <div>
+
+      Find Countries
+
+      <Search onChange={handleChange} placeholder="Search Country"/>
+
+      {countries.length > 10 ? <p>Too many matches</p> : countries.map(country => (
+
+        <li key={country.name.common}>
+
+          {country.name.common}        
+
+          <button onClick={() => handleOnClick(country.name.common)}>Show info</button>  
+
+        </li>
+
+        ))
+
+      }
+
+    </div>  
+
+  )
   
-    const handleChange = event => {
-      setSearchField(event.target.value);
-    }
-
-    const handleOnClick = (name) => {
-      navigate(`../country/${name}`)
-    }
-
-    return(
-      <div>
-              Find Countries
-              <Search onChange={handleChange} placeholder="Search Country"/>
-                {countries.length > 10 ? <p>Too many matches</p> : countries.map(country => (
-                    <li key={country.name.common}>
-                  {country.name.common}        
-                  <button onClick={() => handleOnClick(country.name.common)}>Show info</button>                                                  
-                </li>
-                ))
-              }
-        
-        
-        
-            </div>
-              
-          )
 }
 
 export default CountryList;
